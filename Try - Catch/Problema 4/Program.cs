33c6914d4e4df18f5047a7bd7f7ba85dc7d683b9
NachX90 <nachx90@protonmail.com>
2022-09-11 23:59:47 -0300

    Problema 4

---
+++ Try - Catch/Problema 4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BIENVENIDO");
+            Console.WriteLine("El siguiente programa obtiene las provincias argentinas desde la API");
+            Console.WriteLine("https://apis.datos.gob.ar/georef/api/provincias");
+            //Datos para la conexión a la API
+            var Url = @"https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre";
+            var Conexion = (HttpWebRequest)WebRequest.Create(Url);
+            Conexion.Method = "GET";
+            Conexion.ContentType = "application/json";
+            Conexion.Accept = "application/json";
+
+            //Conexión a la API
+            try
+            {
+                using (WebResponse Respuesta = Conexion.GetResponse())
+                {
+                    using (Stream SR = Respuesta.GetResponseStream())
+                    {
+                        if (SR == null) return;
+                        using (StreamReader SRObjeto = new StreamReader(SR))
+                        {
+                            string RespuestaObjeto = SRObjeto.ReadToEnd();
+                            Root ListadoAPI = JsonSerializer.Deserialize<Root>(RespuestaObjeto);
+                            Console.WriteLine("\nListado de provincias:");
+                            Console.WriteLine("\tID\tProvincia");
+                            
+                            foreach (Provincia Prov in ListadoAPI.Provincias)
+                            {
+                                Console.WriteLine($"\t{Prov.Id}\t{Prov.Nombre}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (WebException Excepcion)
+            {
+                Console.WriteLine("No se pudo acceder a la API.");
+            }
+        }
+    }
+}
+
+
+
+
+
+
